d03cbd3681040e6247b37b783d7a7c7c9961a685
Ethyl <fabianvdmarck@outlook.com>
2026-02-04 16:10:14 +0100

    Commited version 0.5.0 with: - New LatticeConfig.java class - New LatticeConfigurable.java class - Removed redundant ErrUtils.java routing

--- src/main/java/net/ethyl/lattice_api/Config.java
+++
@@ -1,9 +0,0 @@
-package net.ethyl.lattice_api;
-
-import net.neoforged.neoforge.common.ModConfigSpec;
-
-public class Config {
-    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
-
-    static final ModConfigSpec SPEC = BUILDER.build();
-}
--- src/main/java/net/ethyl/lattice_api/LatticeApi.java
+++ src/main/java/net/ethyl/lattice_api/LatticeApi.java
@@ -6,7 +6,6 @@ import net.ethyl.lattice_api.core.instances.Events;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import org.slf4j.Logger;
 
 @Mod(LatticeApi.MOD_ID)
@@ -14,8 +13,7 @@ public class LatticeApi {
     public static final String MOD_ID = "lattice_api";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public LatticeApi(IEventBus modEventBus, ModContainer modContainer) {
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    public LatticeApi(IEventBus modEventBus, ModContainer ignored) {
         LatticeRegistries.register(modEventBus);
         Events.addListener();
     }
